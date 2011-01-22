53bc979585d44ffde31edf8ea3eee61d3dffbbeb
jar.patryk <jar.patryk@138fba30-2b9e-65da-7617-7bae6cb5929c>
2011-01-22 16:53:07 +0000

    dodanie pakietow do zapisu danych w bd

--- src/robotinternetowy/persistence/PersistentSQLite.java
+++ src/robotinternetowy/persistence/PersistentSQLite.java
@@ -4,7 +4,9 @@
 
 package robotinternetowy.persistence;
 
-import java.net.URL;
+import robotinternetowy.persistence.sqlite.DataSrcSqlite;
+import robotinternetowy.persistence.sqlite.data.SQLiteConn;
+import robotinternetowy.utils.Site;
 
 /**
  *
@@ -14,15 +16,61 @@ public class PersistentSQLite implements IPersistent {
 
     private SQLiteConn connection;
 
-    public PersistentSQLite()
+    public PersistentSQLite() throws Exception
     {
         this.connection = new SQLiteConn();
     }
 
-    public void save (URL site)
+    public void save (Site site)
             throws Exception
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        IData src = new DataSrcSqlite(this.connection.getConnection());
+        try
+        {
+            int siteId = src.addSite(site.getAddress());
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+    }
+
+    public void addLink (String url, String link)
+            throws Exception
+    {
+
+    }
+
+    public int howManyLinksAllreadyExistAtThisSite (String link, String url)
+            throws Exception
+    {
+        return 1;
+    }
+
+    public int howManyLinksAllreadyExistAtThisSite (String link, int siteId)
+            throws Exception
+    {
+        return 1;
+    }
+
+     private int getLinksCount (int siteId, String link)
+            throws Exception
+     {
+       
+        return 1;
+     }
+
+    private int getSiteIdByUrl (String url, java.sql.Date date)
+            throws Exception
+    {
+    return 0;
+    }
+
+    public int getSiteIdByUrl (String url)
+            throws Exception
+    {
+        return 1;
     }
 
 }
---
+++ src/robotinternetowy/persistence/sqlite/DataSrcSqlite.java
@@ -0,0 +1,146 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robotinternetowy.persistence.sqlite;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.util.Date;
+import robotinternetowy.persistence.IData;
+
+/**
+ *
+ * @author yarpo
+ */
+public class DataSrcSqlite implements IData
+{
+    public static final String SITES_TABLE_NAME = "sites";
+    public static final String LINKS_TABLE_NAME = "links";
+    public static final int NO_RESULT = -1;
+    protected Connection conn;
+
+    public DataSrcSqlite (Connection conn)
+    {
+        this.conn = conn;
+    }
+
+    public int addSite (String url)
+            throws Exception
+    {
+        String query = "INSERT INTO " + SITES_TABLE_NAME
+                + " (site, date)"
+                + " values (?, ?, ?, ?, ?);";
+        PreparedStatement ps = this.conn.prepareStatement(query);
+        java.sql.Date date = convertDateUtil2Sql(new Date());
+        ps.setString(1, url);
+        ps.setDate(2, date);
+        ps.executeUpdate();
+
+        return getSiteIdByUrl(url, date);
+    }
+
+    public void addLink (String url, String link)
+            throws Exception
+    {
+        if (howManyLinksAllreadyExistAtThisSite(link, url) == NO_RESULT)
+        {
+            throw new Exception("Juz pisano taki link do strony");
+        }
+
+        String query = "INSERT INTO " + LINKS_TABLE_NAME
+                + " (site, date)"
+                + " values (?, ?, ?, ?, ?);";
+        PreparedStatement ps = this.conn.prepareStatement(query);
+        java.sql.Date date = convertDateUtil2Sql(new Date());
+        ps.setString(1, url);
+        ps.setDate(2, date);
+        ps.executeUpdate();
+    }
+
+    public int howManyLinksAllreadyExistAtThisSite (String link, String url)
+            throws Exception
+    {
+        int id = getSiteIdByUrl(url);
+        return getLinksCount(id, link);
+    }
+
+    public int howManyLinksAllreadyExistAtThisSite (String link, int siteId)
+            throws Exception
+    {
+        return getLinksCount(siteId, link);
+    }
+
+     private int getLinksCount (int siteId, String link)
+            throws Exception
+     {
+        String query = "SELECT count(id) as number FROM " + LINKS_TABLE_NAME
+                + " WHERE id_site = ? AND site_address = ?";
+        PreparedStatement ps = this.conn.prepareStatement(query);
+        ps.setInt(1, siteId);
+        ps.setString(2, link);
+
+        ResultSet result = ps.executeQuery();
+        int count = NO_RESULT;
+
+        if (result.next())
+        {
+            count = result.getInt("number");
+        }
+
+        result.close();
+        ps.close();
+
+        return count;
+     }
+
+    private int getSiteIdByUrl (String url, java.sql.Date date)
+            throws Exception
+    {
+        String query = "SELECT id FROM " + SITES_TABLE_NAME
+                + " WHERE site = ? AND date = ?";
+        PreparedStatement ps = this.conn.prepareStatement(query);
+        ps.setString(1, url);
+        ps.setDate(2, date);
+
+        ResultSet result = ps.executeQuery();
+        int id = NO_RESULT;
+
+        if (result.next())
+        {
+            id = result.getInt("id");
+        }
+
+        result.close();
+        ps.close();
+
+        return id;
+    }
+
+    public int getSiteIdByUrl (String url)
+            throws Exception
+    {
+        String query = "SELECT id FROM " + SITES_TABLE_NAME
+                + " WHERE site = ?";
+        PreparedStatement ps = this.conn.prepareStatement(query);
+        ps.setString(1, url);
+        ResultSet result = ps.executeQuery();
+        int id = NO_RESULT;
+
+        if (result.next())
+        {
+            id = result.getInt("id");
+        }
+
+        result.close();
+        ps.close();
+
+        return id;
+    }
+
+    private java.sql.Date convertDateUtil2Sql (Date date)
+    {
+        return new java.sql.Date(date.getTime());
+    }
+}
