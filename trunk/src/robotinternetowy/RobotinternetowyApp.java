9dcba639aafb5703afe2331f7e7b280d6151c65a
jar.patryk <jar.patryk@138fba30-2b9e-65da-7617-7bae6cb5929c>
2011-01-22 14:16:12 +0000

    porawki w wygladzie interfejsu

--- trunk/src/robotinternetowy/RobotinternetowyApp.java
+++ trunk/src/robotinternetowy/RobotinternetowyApp.java
@@ -1,7 +1,6 @@
 /*
  * RobotinternetowyApp.java
  */
-
 package robotinternetowy;
 
 import java.io.BufferedReader;
@@ -14,12 +13,14 @@ import org.jdesktop.application.SingleFrameApplication;
 /**
  * The main class of the application.
  */
-public class RobotinternetowyApp extends SingleFrameApplication {
-
+public class RobotinternetowyApp extends SingleFrameApplication
+{
     /**
      * At startup create and show the main frame of the application.
      */
-    @Override protected void startup() {
+    @Override
+    protected void startup ()
+    {
         show(new RobotinternetowyView(this));
     }
 
@@ -28,33 +29,40 @@ public class RobotinternetowyApp extends SingleFrameApplication {
      * Windows shown in our application come fully initialized from the GUI
      * builder, so this additional configuration is not needed.
      */
-    @Override protected void configureWindow(java.awt.Window root) {
+    @Override
+    protected void configureWindow (java.awt.Window root)
+    {
     }
 
     /**
      * A convenient static getter for the application instance.
      * @return the instance of RobotinternetowyApp
      */
-    public static RobotinternetowyApp getApplication() {
+    public static RobotinternetowyApp getApplication ()
+    {
         return Application.getInstance(RobotinternetowyApp.class);
     }
 
     /**
      * Main method launching the application.
      */
-    public static void main(String[] args)throws Exception {
+    public static void main (String[] args)
+            throws Exception
+    {
         launch(RobotinternetowyApp.class, args);
         System.out.println("Działa");
         URL site = new URL("http://eti.dragoart.info/index.php");
 
         URLConnection yc = site.openConnection();
         BufferedReader in = new BufferedReader(
-                                new InputStreamReader(
-                                yc.getInputStream()));
+                new InputStreamReader(
+                yc.getInputStream()));
         String inputLine;
 
         while ((inputLine = in.readLine()) != null)
+        {
             System.out.println(inputLine);
+        }
         in.close();
     }
 }
