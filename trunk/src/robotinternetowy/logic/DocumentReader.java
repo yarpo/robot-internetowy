b8e2cef806e2244152c3b54585487a0f630f740a
jar.patryk <jar.patryk@138fba30-2b9e-65da-7617-7bae6cb5929c>
2011-01-23 16:43:42 +0000

    pobieranie daych od uzytkownika i ich walidacja

--- trunk/src/robotinternetowy/logic/DocumentReader.java
+++ trunk/src/robotinternetowy/logic/DocumentReader.java
@@ -1,18 +1,25 @@
 /*
  * Klasa odpowiedzialna za watki
  */
-
 package robotinternetowy.logic;
+
+import robotinternetowy.utils.RemoteFile;
 
 /**
  *
  * @author yarpo
  */
-public class DocumentReader implements Runnable {
+public class DocumentReader implements Runnable
+{
+    private RemoteFile file;
+
+    public DocumentReader(RemoteFile rfile)
+    {
+        file = rfile;
+    }
 
     public void run ()
     {
         
     }
-
 }
