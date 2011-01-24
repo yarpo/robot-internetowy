0643900a05e7d2d2a00e428e116ca5c9bf93f5ef
jar.patryk <jar.patryk@138fba30-2b9e-65da-7617-7bae6cb5929c>
2011-01-24 00:19:56 +0000

    poprawka regexpa na linki wraz z testami

---
+++ trunk/test/robotinternetowy/logic/helpers/HyperLinksFetcherTest.java
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package robotinternetowy.logic.helpers;
+
+import java.util.ArrayList;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author yarpo
+ */
+public class HyperLinksFetcherTest
+{
+    public HyperLinksFetcherTest ()
+    {
+    }
+
+    @BeforeClass
+    public static void setUpClass ()
+            throws Exception
+    {
+    }
+
+    @AfterClass
+    public static void tearDownClass ()
+            throws Exception
+    {
+    }
+
+    @Test
+    public void testGet_simple_ok ()
+            throws Exception
+    {
+        new HLFTestBulder(
+                "<a href=\"test\">tekst</a>\n<a href=\"test2\">tekst</a>").
+                expectedResultsNo(2).
+                atPositionIsValue(0, "test").
+                atPositionIsValue(1, "test2");
+    }
+
+    @Test
+    public void testGet_badHTML_Result0 ()
+            throws Exception
+    {
+        new HLFTestBulder("<a href='test'tekst</a>").expectedResultsNo(0);
+    }
+
+    @Test
+    public void testGet_validHTML_Result0 ()
+            throws Exception
+    {
+        new HLFTestBulder("<p href=\"ssadasd\">").expectedResultsNo(0);
+    }
+
+    @Test
+    public void testGet_validHTMLwithAtrr_Result1 ()
+            throws Exception
+    {
+        new HLFTestBulder("<p><a clas=\"sdsd sdsa aa\" name=\"dsfgdf\" href=\"aaaaa\" id=\"aaaa\"></a></p>").
+                expectedResultsNo(1).
+                atPositionIsValue(0, "aaaaa");
+    }
+
+    @Test
+    public void testGet_validHTMLwithSingleQoute_Result1 ()
+            throws Exception
+    {
+        new HLFTestBulder("<p><a href='aaaaa' id=\"aaaa\"></a></p>").
+                expectedResultsNo(1).
+                atPositionIsValue(0, "aaaaa");
+    }
+}
+class HLFTestBulder
+{
+    private ArrayList<String> result;
+
+    public HLFTestBulder (String code)
+            throws Exception
+    {
+        HyperLinksFetcher fetcher = new HyperLinksFetcher(code);
+        result = fetcher.get();
+    }
+
+    public HLFTestBulder atPositionIsValue (int position, String value)
+    {
+        assertEquals(result.get(position), value);
+        return this;
+    }
+
+    public HLFTestBulder expectedResultsNo (int n)
+    {
+        assertEquals(result.size(), n);
+        return this;
+    }
+}
