ff59a3d41c0d014d51ff1a57d509ed12baa0a47b
jar.patryk <jar.patryk@138fba30-2b9e-65da-7617-7bae6cb5929c>
2011-01-31 16:14:44 +0000

    zapisuje do bazy danych

--- trunk/src/robotinternetowy/logic/Logic.java
+++ trunk/src/robotinternetowy/logic/Logic.java
@@ -3,6 +3,7 @@
  */
 package robotinternetowy.logic;
 
+import robotinternetowy.logic.document.Reader;
 import java.util.ArrayList;
 import robotinternetowy.PopupDialog;
 import robotinternetowy.logger.ILogger;
@@ -15,33 +16,19 @@ public class Logic
 {
     private Settings settings;
     private ILogger logger;
-    private int documentsLimit;
-    private int documentsRead;
 
     public Logic (Settings set, ILogger log)
     {
         settings = set;
         logger = log;
-        documentsLimit = settings.getDepth();
-        documentsRead = 0;
     }
-
-
 
     public void start () throws Exception
     {
         logger.log("Startowy url: " + settings.getUrl());
+        Reader.setLimitOfDocuments(settings.getDepth());
+        Reader.setLogger(logger);
         start(settings.getUrl());
-
-        // pobrac wszystkie dane
-        // rozpoczac watek odczytujacy pierwsza strone
-        // po znalezieniu nastepnej strony rozpoczac dla niej watek,
-        //      jesli jest to dokument dozwolonego typu
-    }
-
-    private synchronized void increaseDocumentsCounter()
-    {
-        documentsRead++;
     }
 
     public void start (String url) throws Exception
@@ -50,9 +37,8 @@ public class Logic
 
         if (document.isContentTypeAllowed())
         {
-            increaseDocumentsCounter();
             logger.log("\tObrabiam plik : " + document.getAddressWithProtocol());
-            DocumentReader docReader = new DocumentReader(document);
+            Reader docReader = new Reader(document);
             (new Thread(docReader)).start();
         }
         else
