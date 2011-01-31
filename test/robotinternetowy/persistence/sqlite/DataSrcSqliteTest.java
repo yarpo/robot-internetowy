c0c930ff9d6558282dcff25113b76c10d7b9ac7f
jar.patryk <jar.patryk@138fba30-2b9e-65da-7617-7bae6cb5929c>
2011-01-31 12:09:12 +0000

    testy + poprawki. działa

--- test/robotinternetowy/persistence/sqlite/DataSrcSqliteTest.java
+++ test/robotinternetowy/persistence/sqlite/DataSrcSqliteTest.java
@@ -8,6 +8,7 @@ package robotinternetowy.persistence.sqlite;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import robotinternetowy.persistence.IData;
 import static org.junit.Assert.*;
 
 /**
@@ -23,29 +24,30 @@ public class DataSrcSqliteTest {
     public static void setUpClass ()
             throws Exception
     {
+        DataSrcSqlite data = new DataSrcSqlite((new SQLiteConn()).getConnection());
+        data.trunk();
     }
 
     @AfterClass
     public static void tearDownClass ()
             throws Exception
     {
+        DataSrcSqlite data = new DataSrcSqlite((new SQLiteConn()).getConnection());
+        data.trunk();
     }
 
     /**
      * Test of addSite method, of class DataSrcSqlite.
      */
     @Test
-    public void testAddSite ()
+    public void testAddSite_ok ()
             throws Exception
     {
-        System.out.println("addSite");
-        String url = "";
-        DataSrcSqlite instance = null;
-        int expResult = 0;
-        int result = instance.addSite(url);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        IData data = new DataSrcSqlite((new SQLiteConn()).getConnection());
+        String url = "http://yarpo.pl";
+        int res1 =  data.addSite(url);
+        int res2 = data.getSiteIdByUrl(url);
+        assertEquals(res1, res2);
     }
 
     /**
@@ -55,82 +57,28 @@ public class DataSrcSqliteTest {
     public void testAddLink_String_String ()
             throws Exception
     {
-        System.out.println("addLink");
-        String url = "";
-        String link = "";
-        DataSrcSqlite instance = null;
-        instance.addLink(url, link);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        IData data = new DataSrcSqlite((new SQLiteConn()).getConnection());
+        String url = "http://yarpo.pl/";
+        String link = url + "plik.html";
+        data.addSite(url);
+        data.addLink(url, link);
+        int res = data.howManyLinksAlreadyExistAtThisSite(link, url);
+        assertEquals(res, 1);
     }
 
     /**
      * Test of addLink method, of class DataSrcSqlite.
      */
     @Test
-    public void testAddLink_int_String ()
+    public void testAddLink_Int_String ()
             throws Exception
     {
-        System.out.println("addLink");
-        int fromDocument = 0;
-        String link = "";
-        DataSrcSqlite instance = null;
-        instance.addLink(fromDocument, link);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        IData data = new DataSrcSqlite((new SQLiteConn()).getConnection());
+        String url = "http://yarpo.pl/";
+        String link = url + "plik.html";
+        int id = data.addSite(url);
+        data.addLink(id, link);
+        int res = data.howManyLinksAlreadyExistAtThisSite(link, url);
+        assertEquals(res, 1);
     }
-
-    /**
-     * Test of howManyLinksAllreadyExistAtThisSite method, of class DataSrcSqlite.
-     */
-    @Test
-    public void testHowManyLinksAllreadyExistAtThisSite_String_String ()
-            throws Exception
-    {
-        System.out.println("howManyLinksAllreadyExistAtThisSite");
-        String link = "";
-        String url = "";
-        DataSrcSqlite instance = null;
-        int expResult = 0;
-        int result = instance.howManyLinksAlreadyExistAtThisSite(link, url);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of howManyLinksAllreadyExistAtThisSite method, of class DataSrcSqlite.
-     */
-    @Test
-    public void testHowManyLinksAllreadyExistAtThisSite_String_int ()
-            throws Exception
-    {
-        System.out.println("howManyLinksAllreadyExistAtThisSite");
-        String link = "";
-        int siteId = 0;
-        DataSrcSqlite instance = null;
-        int expResult = 0;
-        int result = instance.howManyLinksAlreadyExistAtThisSite(link, siteId);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getSiteIdByUrl method, of class DataSrcSqlite.
-     */
-    @Test
-    public void testGetSiteIdByUrl ()
-            throws Exception
-    {
-        System.out.println("getSiteIdByUrl");
-        String url = "";
-        DataSrcSqlite instance = null;
-        int expResult = 0;
-        int result = instance.getSiteIdByUrl(url);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
 }
